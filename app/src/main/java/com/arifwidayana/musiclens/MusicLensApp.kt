b72a3e3ad58e54bd6bfac36535b304a67f4c2f8a
Arif Widayana <arifwidayana@gmail.com>
2023-07-14 22:23:10 +0700

    feat: added timber and koin injection

---
+++ app/src/main/java/com/arifwidayana/musiclens/MusicLensApp.kt
@@ -0,0 +1,22 @@
+package com.arifwidayana.musiclens
+
+import android.app.Application
+import org.koin.android.BuildConfig
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import timber.log.Timber
+
+class MusicLensApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+        startKoin {
+            androidLogger()
+            androidContext(this@MusicLensApp)
+            modules(
+
+            )
+        }
+    }
+}
