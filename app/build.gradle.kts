a5fbd3d8f6136372e88fc797bc4a97b35af8d2a4
Arif Widayana <arifwidayana@gmail.com>
2023-07-14 21:27:00 +0700

    setup gradle dependencies include ktlint

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,11 @@
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.kotlinKapt)
+    alias(libs.plugins.ktlintAndroid)
 }
 
 android {
@@ -36,8 +40,73 @@ android {
     }
 }
 
-dependencies {
+tasks.getByPath("preBuild").dependsOn("ktlintFormat")
 
+configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+    debug.set(true)
+    verbose.set(true)
+    android.set(false)
+    outputToConsole.set(true)
+    outputColorName.set("RED")
+    ignoreFailures.set(true)
+    enableExperimentalRules.set(true)
+    additionalEditorconfigFile.set(file("/some/additional/.editorconfig"))
+    disabledRules.set(setOf("final-newline", "no-wildcard-imports"))
+    baseline.set(file("my-project-ktlint-baseline.xml"))
+    reporters {
+        reporter(ReporterType.PLAIN)
+        reporter(ReporterType.CHECKSTYLE)
+    }
+    kotlinScriptAdditionalPaths {
+        include(fileTree("scripts/"))
+    }
+    filter {
+        exclude("**/generated/**")
+        include("**/kotlin/**")
+    }
+}
+
+dependencies {
+    // Lifecycle
+    implementation(libs.lifecycle.runtime)
+    implementation(libs.lifecycle.viewmodel)
+
+    // Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.gson)
+
+    // Coroutine
+    implementation(libs.coroutines.core)
+    implementation(libs.coroutines.android)
+
+    // Datastore
+    implementation(libs.datastore)
+
+    // Gson
+    implementation(libs.gson)
+
+    // Koin
+    implementation(libs.koin)
+
+    // Paging
+    implementation(libs.paging.runtime)
+
+    // Coil
+    implementation(libs.coil)
+
+    // Mock
+    implementation(libs.mock)
+
+    // Chucker
+    debugImplementation(libs.chucker)
+    releaseImplementation(libs.chucker.no.op)
+
+    // Room
+    implementation(libs.room.ktx)
+    implementation(libs.room.runtime)
+    kapt(libs.room.compiler)
+
+    // Default
     implementation(libs.core.ktx)
     implementation(libs.appcompat)
     implementation(libs.material)
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.kotlinKapt) apply false
+    alias(libs.plugins.ktlintAndroid) apply false
 }
 true // Needed to make the Suppress annotation work for the plugins block
