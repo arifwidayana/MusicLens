66c75f289ef026faaea591a91ff0eeaa9f9a90db
Arif Widayana <arifwidayana@gmail.com>
2023-07-15 15:12:37 +0700

    feat: added repository child of base repository

---
+++ app/src/main/java/com/arifwidayana/musiclens/data/repository/Repository.kt
@@ -0,0 +1,26 @@
+package com.arifwidayana.musiclens.data.repository
+
+import com.arifwidayana.musiclens.arch.base.BaseRepository
+import com.arifwidayana.musiclens.arch.base.BaseResponse
+import com.google.gson.Gson
+import com.google.gson.JsonParseException
+import okhttp3.ResponseBody
+import org.koin.java.KoinJavaComponent
+
+/**
+ * Repository child of BaseRepository using for
+ * catching the error and throw error message
+ */
+open class Repository : BaseRepository() {
+    private val gson: Gson by KoinJavaComponent.inject(Gson::class.java)
+
+    override fun <T> getErrorMessageFromApi(response: T): String {
+        val responseBody = response as ResponseBody
+        return try {
+            val body = gson.fromJson(responseBody.string(), BaseResponse::class.java)
+            body.message ?: "Error Api"
+        } catch (e: JsonParseException) {
+            "Error Api"
+        }
+    }
+}
