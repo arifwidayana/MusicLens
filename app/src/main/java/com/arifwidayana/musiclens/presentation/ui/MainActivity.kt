b66c53a0e31009af8688287683ebf7e4574579cc
Arif Widayana <arifwidayana@gmail.com>
2023-07-16 22:57:24 +0700

    feat: updated main activity with several action

--- app/src/main/java/com/arifwidayana/musiclens/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/arifwidayana/musiclens/presentation/ui/MainActivity.kt
@@ -1,44 +1,105 @@
 package com.arifwidayana.musiclens.presentation.ui
 
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.arifwidayana.musiclens.arch.base.BaseActivity
+import com.arifwidayana.musiclens.arch.utils.ext.changed
+import com.arifwidayana.musiclens.data.network.model.response.MusicParamResponse
 import com.arifwidayana.musiclens.databinding.ActivityMainBinding
+import com.arifwidayana.musiclens.presentation.adapter.MusicAdapter
+import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
+import timber.log.Timber
 
 class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(
     ActivityMainBinding::inflate
 ) {
     override val viewModel: MainViewModel by inject()
-    private val audioUrl = listOf(
-        "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/fd/bb/38/fdbb38d2-073d-4bc7-68c4-348a0be6d560/mzaf_4150435585996894188.plus.aac.p.m4a",
-        "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-5.mp3",
-        "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-4.mp3"
-        )
-    private var audioPosition = 0
-
+    private var currentMusic = 0
+    private var adapter: MusicAdapter? = null
     override fun initView() {
+        onView()
+        onClick()
+    }
+
+    private fun onView() {
         binding.apply {
+            svArtistMusic.changed(onQueryTextChange = {
+                viewModel.onEvent(MainEvent.SearchQueryChange(it))
+            })
+        }
+    }
+
+    private fun onClick() {
+        binding.apply {
+            binding.sbMusic.changed(onProgressChanged = { seekbar, progress, fromUser ->
+                viewModel.progressSeekbarMusic(seekbar, progress, fromUser)
+            })
             btnPrevious.setOnClickListener {
-                if (audioPosition > 0) {
-                    audioPosition--
-                    playMusic()
-                }
+                viewModel.previousMusic(
+                    currentMusic = --currentMusic,
+                    seekBar = sbMusic
+                )
+                selectedMusic()
             }
             btnPlay.setOnClickListener {
-                playMusic()
+                viewModel.playOrPause(
+                    currentMusic = currentMusic,
+                    seekBar = sbMusic
+                )
+                selectedMusic()
             }
             btnNext.setOnClickListener {
-                if (audioPosition < audioUrl.size) {
-                    audioPosition++
-                    playMusic()
+                viewModel.nextMusic(
+                    currentMusic = ++currentMusic,
+                    seekBar = sbMusic
+                )
+                selectedMusic()
+            }
+        }
+    }
+
+    /**
+     * observe data from view model using lifecycle
+     */
+    override fun observeData() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.CREATED) {
+                viewModel.state.collect {
+                    setMusicAdapter(it.listMusic)
+                    setIconButton(it.iconButton)
+                    Timber.tag("E").e(it.errorMessage)
                 }
             }
         }
     }
 
-    override fun observeData() {
+    /**
+     * set icon button play and pause
+     */
+    private fun setIconButton(setIcon: Int) {
+        binding.btnPlay.setImageResource(setIcon)
     }
 
-    private fun playMusic() {
-        viewModel.playAndPauseMusic(audioUrl[audioPosition])
+    /**
+     * set data to adapter using submit list and invoke click using lambda in adapter
+     */
+    private fun setMusicAdapter(listMusic: List<MusicParamResponse>) {
+        adapter = MusicAdapter {
+            currentMusic = it
+            viewModel.playOrPause(
+                currentMusic = currentMusic,
+                seekBar = binding.sbMusic
+            )
+            selectedMusic()
+        }
+        binding.rvMusic.adapter = adapter
+        adapter?.submitList(listMusic)
     }
+
+    /**
+     * every click item music in adapter will be update the music
+     */
+    private fun selectedMusic() = adapter?.selectMusic(currentMusic)
 }
