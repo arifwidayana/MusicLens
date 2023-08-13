b5c1962b07ec118c9b6651683c63171fea4d018b
Arif Widayana <arifwidayana@gmail.com>
2023-08-13 21:55:08 +0700

    refactor: change injector Koin to Dagger2

--- app/src/main/java/com/arifwidayana/musiclens/MusicLensApp.kt
+++ app/src/main/java/com/arifwidayana/musiclens/MusicLensApp.kt
@@ -1,23 +1,25 @@
 package com.arifwidayana.musiclens
 
 import android.app.Application
-import com.arifwidayana.musiclens.di.MusicLensModule
+import com.arifwidayana.musiclens.di.component.DaggerAppComponent
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasAndroidInjector
 import org.koin.android.BuildConfig
-import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
-import org.koin.core.context.startKoin
 import timber.log.Timber
+import javax.inject.Inject
 
-class MusicLensApp: Application() {
+class MusicLensApp: Application(), HasAndroidInjector {
+    @Inject
+    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
+
+    override fun androidInjector(): AndroidInjector<Any> {
+        return dispatchingAndroidInjector
+    }
+
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
-        startKoin {
-            androidLogger()
-            androidContext(this@MusicLensApp)
-            modules(
-                MusicLensModule.getModules()
-            )
-        }
+        DaggerAppComponent.builder().application(this).build().inject(this)
     }
 }
